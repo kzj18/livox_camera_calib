e4499badd785a9b08c45aa754b9c7faa810c448c
kzj18 <kzj18@qq.com>
2024-09-02 20:11:15 +0800

    build successfully

--- include/common.h
+++ include/common.h
@@ -1,8 +1,8 @@
 #ifndef LIDAR_CAMERA_COMMON_H
 #define LIDAR_CAMERA_COMMON_H
-#include <Eigen/Core>
-#include <cv_bridge/cv_bridge.h>
-#include <pcl/common/io.h>
+#include "Eigen/Core"
+#include "cv_bridge/cv_bridge.h"
+#include "pcl/common/io.h"
 #include <stdio.h>
 #include <string>
 #include <unordered_map>
